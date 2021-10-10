8de86857acc5aca31148eb20856604e9529a6713
JPEdgar <70185831+JPEdgar@users.noreply.github.com>
2021-10-10 15:54:11 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import ReactDOM from "react-dom";
 // custom components
 import App from "./App";
 
+function Test () {
+   return (
+      <h1>Test page</h1>
+   )
+}
+
 ReactDOM.render(
    
-      <App />
+      <Test />
    ,
    document.getElementById("root")
 );
