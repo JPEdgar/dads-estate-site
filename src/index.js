56d08f1b7e9b0fe16751026416944a06d979dbec
JPEdgar <sevisin@hotmail.com>
2021-10-10 20:30:26 -0800

    10/10/2021, github deploy test, 830p

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import ReactDOM from "react-dom";
 // custom components
 import App from "./App";
 
-function Test () {
-   return (
-      <h1>Test page</h1>
-   )
-}
-
 ReactDOM.render(
    
-      <Test />
+   <h1>Test page</h1>
    ,
    document.getElementById("root")
 );
