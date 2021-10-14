9d296382ebda117addd9f35d204124c8c9f113aa
JPEdgar <sevisin@hotmail.com>
2021-10-14 14:25:21 -0800

    10/14/21, Version 1.0

--- src/constants/RoomsConstants.js
+++ src/constants/RoomsConstants.js
@@ -1,4 +1,8 @@
 // rooms images
+import OutsideNorth from "../images/locations/Location-Outside-North.jpg"
+import OutsideEast from "../images/locations/Location-Outside-East.jpg"
+import OutsideSouth from "../images/locations/Location-Outside-South.jpg"
+import OutsideWest from "../images/locations/Location-Outside-West.jpg"
 // import F1_Outline from "../images/locations/Floor1-Outline.jpg";
 import F1_Garage from "../images/locations/Floor1-Garage.jpg";
 import F1_Entry from "../images/locations/Floor1-Entry.jpg";
@@ -16,24 +20,170 @@ import F2_Bath from "../images/locations/Floor2-Bathroom.jpg";
 import F2_Bed from "../images/locations/Floor2-Bedroom.jpg";
 import F2_Storage from "../images/locations/Floor2-Storage.jpg";
 
-// sections images - garage
-import test1 from "../images/01.jpg";
-import test2 from "../images/02.jpg";
-import test3 from "../images/03.jpg";
-import test4 from "../images/04.jpg";
+// section images - outside (10/11/21)
+import OutsideEast01 from "../images/Outside/Outside-East01_10-14-21.JPG"
+import OutsideEast02 from "../images/Outside/Outside-East02_10-14-21.JPG"
+import OutsideEast03 from "../images/Outside/Outside-East03_10-14-21.JPG"
+import OutsideEast04 from "../images/Outside/Outside-East04_10-14-21.JPG"
+import OutsideEast05 from "../images/Outside/Outside-East05_10-14-21.JPG"
+import OutsideEast06 from "../images/Outside/Outside-East06_10-14-21.JPG"
+import OutsideNorth01 from "../images/Outside/Outside-North01_10-14-21.JPG"
+import OutsideSouth01 from "../images/Outside/Outside-South01_10-14-21.JPG"
+import OutsideSouth02 from "../images/Outside/Outside-South02_10-14-21.JPG"
+import OutsideSouth03 from "../images/Outside/Outside-South03_10-14-21.JPG"
+import OutsideSouth04 from "../images/Outside/Outside-South04_10-14-21.JPG"
+import OutsideSouth05 from "../images/Outside/Outside-South05_10-14-21.JPG"
+import OutsideSouth06 from "../images/Outside/Outside-South06_10-14-21.JPG"
+import OutsideSouth07 from "../images/Outside/Outside-South07_10-14-21.JPG"
+import OutsideWest01 from "../images/Outside/Outside-West01_10-14-21.JPG"
+import OutsideWest02 from "../images/Outside/Outside-West02_10-14-21.JPG"
 
-// section images - kitchen (10/4/2021)
-import Kitchen01 from "../images/Kitchen01_10-4-2021.JPG"
-import Kitchen02 from "../images/Kitchen02_10-4-2021.JPG"
-import Kitchen03 from "../images/Kitchen03_10-4-2021.JPG"
-import Kitchen04 from "../images/Kitchen04_10-4-2021.JPG"
-import Kitchen05 from "../images/Kitchen05_10-4-2021.JPG"
-import Kitchen06 from "../images/Kitchen06_10-4-2021.JPG"
-import Kitchen07 from "../images/Kitchen07_10-4-2021.JPG"
+// sections images - garage (10/14/21)
+import Garage01 from "../images/Garage/Garage01_10-14-21.JPG"
+import Garage02 from "../images/Garage/Garage02_10-14-21.JPG"
+import Garage03 from "../images/Garage/Garage03_10-14-21.JPG"
+import Garage04 from "../images/Garage/Garage04_10-14-21.JPG"
+import Garage05 from "../images/Garage/Garage05_10-14-21.JPG"
+import Garage06 from "../images/Garage/Garage06_10-14-21.JPG"
+import Garage07 from "../images/Garage/Garage07_10-14-21.JPG"
+import Garage08 from "../images/Garage/Garage08_10-14-21.JPG"
+import Garage09 from "../images/Garage/Garage09_10-14-21.JPG"
+import Garage10 from "../images/Garage/Garage10_10-14-21.JPG"
+import Garage11 from "../images/Garage/Garage11_10-14-21.JPG"
+import Garage12 from "../images/Garage/Garage12_10-14-21.JPG"
+
+// section images - entry (10/14/21)
+import Entry01 from "../images/Entry/Entry01_10-14-21.JPG"
+import Entry02 from "../images/Entry/Entry02_10-14-21.JPG"
+
+// section images - kitchen (10/4/21)
+import Kitchen01 from "../images/Kitchen/Kitchen01_10-4-2021.JPG"
+import Kitchen02 from "../images/Kitchen/Kitchen02_10-4-2021.JPG"
+import Kitchen03 from "../images/Kitchen/Kitchen03_10-4-2021.JPG"
+import Kitchen04 from "../images/Kitchen/Kitchen04_10-4-2021.JPG"
+import Kitchen05 from "../images/Kitchen/Kitchen05_10-4-2021.JPG"
+import Kitchen06 from "../images/Kitchen/Kitchen06_10-4-2021.JPG"
+import Kitchen07 from "../images/Kitchen/Kitchen07_10-4-2021.JPG"
+
+// section images - dining room (10/14/21)
+import DiningRoom01 from "../images/DiningRoom/DiningRoom01_10-14-21.JPG"
+import DiningRoom02 from "../images/DiningRoom/DiningRoom02_10-14-21.JPG"
+import DiningRoom03 from "../images/DiningRoom/DiningRoom03_10-14-21.JPG"
+import DiningRoom04 from "../images/DiningRoom/DiningRoom04_10-14-21.JPG"
+import DiningRoom05 from "../images/DiningRoom/DiningRoom05_10-14-21.JPG"
+import DiningRoom06 from "../images/DiningRoom/DiningRoom06_10-14-21.JPG"
+import DiningRoom07 from "../images/DiningRoom/DiningRoom07_10-14-21.JPG"
+import DiningRoom08 from "../images/DiningRoom/DiningRoom08_10-14-21.JPG"
+import DiningRoom09 from "../images/DiningRoom/DiningRoom09_10-14-21.JPG"
+import DiningRoom10 from "../images/DiningRoom/DiningRoom10_10-14-21.JPG"
+import DiningRoom11 from "../images/DiningRoom/DiningRoom11_10-14-21.JPG"
+import DiningRoom12 from "../images/DiningRoom/DiningRoom12_10-14-21.JPG"
+import DiningRoom13 from "../images/DiningRoom/DiningRoom13_10-14-21.JPG"
+import DiningRoom14 from "../images/DiningRoom/DiningRoom14_10-14-21.JPG"
+
+// section images - living room (10/14/21)
+import LivingRoom01 from "../images/LivingRoom/LivingRoom01_10-14-21.JPG"
+import LivingRoom02 from "../images/LivingRoom/LivingRoom02_10-14-21.JPG"
+import LivingRoom03 from "../images/LivingRoom/LivingRoom03_10-14-21.JPG"
+import LivingRoom04 from "../images/LivingRoom/LivingRoom04_10-14-21.JPG"
+import LivingRoom05 from "../images/LivingRoom/LivingRoom05_10-14-21.JPG"
+import LivingRoom06 from "../images/LivingRoom/LivingRoom06_10-14-21.JPG"
+import LivingRoom07 from "../images/LivingRoom/LivingRoom07_10-14-21.JPG"
+import LivingRoom08 from "../images/LivingRoom/LivingRoom08_10-14-21.JPG"
+import LivingRoom09 from "../images/LivingRoom/LivingRoom09_10-14-21.JPG"
+import LivingRoom10 from "../images/LivingRoom/LivingRoom10_10-14-21.JPG"
+import LivingRoom11 from "../images/LivingRoom/LivingRoom11_10-14-21.JPG"
+import LivingRoom12 from "../images/LivingRoom/LivingRoom12_10-14-21.JPG"
+import LivingRoom13 from "../images/LivingRoom/LivingRoom13_10-14-21.JPG"
+import LivingRoom14 from "../images/LivingRoom/LivingRoom14_10-14-21.JPG"
+import LivingRoom15 from "../images/LivingRoom/LivingRoom15_10-14-21.JPG"
+import LivingRoom16 from "../images/LivingRoom/LivingRoom16_10-14-21.JPG"
+import LivingRoom17 from "../images/LivingRoom/LivingRoom17_10-14-21.JPG"
+import LivingRoom18 from "../images/LivingRoom/LivingRoom18_10-14-21.JPG"
+import LivingRoom19 from "../images/LivingRoom/LivingRoom19_10-14-21.JPG"
+import LivingRoom20 from "../images/LivingRoom/LivingRoom20_10-14-21.JPG"
+import LivingRoom21 from "../images/LivingRoom/LivingRoom21_10-14-21.JPG"
+
+// section images - downstairs hall (10/14/21)
+import DownstairsHall01 from "../images/DownstairsHall/DownstairsHall01_10-14-21.JPG"
+import DownstairsHall02 from "../images/DownstairsHall/DownstairsHall02_10-14-21.JPG"
+import DownstairsHall03 from "../images/DownstairsHall/DownstairsHall03_10-14-21.JPG"
+import DownstairsHall04 from "../images/DownstairsHall/DownstairsHall04_10-14-21.JPG"
+import DownstairsHall05 from "../images/DownstairsHall/DownstairsHall05_10-14-21.JPG"
+import DownstairsHall06 from "../images/DownstairsHall/DownstairsHall06_10-14-21.JPG"
+import DownstairsHall07 from "../images/DownstairsHall/DownstairsHall07_10-14-21.JPG"
+import DownstairsHall08 from "../images/DownstairsHall/DownstairsHall08_10-14-21.JPG"
+import DownstairsHall09 from "../images/DownstairsHall/DownstairsHall09_10-14-21.JPG"
+
+// section images - office (10/14/21)
+import Office01 from "../images/Office/Office01_10-14-21.JPG"
+import Office02 from "../images/Office/Office02_10-14-21.JPG"
+import Office03 from "../images/Office/Office03_10-14-21.JPG"
+import Office04 from "../images/Office/Office04_10-14-21.JPG"
+import Office05 from "../images/Office/Office05_10-14-21.JPG"
+import Office06 from "../images/Office/Office06_10-14-21.JPG"
+import Office07 from "../images/Office/Office07_10-14-21.JPG"
+import Office08 from "../images/Office/Office08_10-14-21.JPG"
+
+// section images - downstairs bathroom (10/14/21)
+import DownstairsBath01 from "../images/DownstairsBath/DownstairsBath01_10-14-21.JPG"
+import DownstairsBath02 from "../images/DownstairsBath/DownstairsBath02_10-14-21.JPG"
+import DownstairsBath03 from "../images/DownstairsBath/DownstairsBath03_10-14-21.JPG"
+import DownstairsBath04 from "../images/DownstairsBath/DownstairsBath04_10-14-21.JPG"
+import DownstairsBath05 from "../images/DownstairsBath/DownstairsBath05_10-14-21.JPG"
+
+// section images - stairs (10/14/21)
+import Stairs01 from "../images/Stairs/Stairs01_10-14-21.JPG"
+import Stairs02 from "../images/Stairs/Stairs02_10-14-21.JPG"
+import Stairs03 from "../images/Stairs/Stairs03_10-14-21.JPG"
+import Stairs04 from "../images/Stairs/Stairs04_10-14-21.JPG"
+import Stairs05 from "../images/Stairs/Stairs05_10-14-21.JPG"
+import Stairs06 from "../images/Stairs/Stairs06_10-14-21.JPG"
+import Stairs07 from "../images/Stairs/Stairs07_10-14-21.JPG"
+
+// section images - upstairs hallway (10/14/2021)
+import UpstairsHall01 from "../images/UpstairsHall/UpstairsHall01_10-14-21.JPG"
+import UpstairsHall02 from "../images/UpstairsHall/UpstairsHall02_10-14-21.JPG"
+import UpstairsHall03 from "../images/UpstairsHall/UpstairsHall03_10-14-21.JPG"
+import UpstairsHall04 from "../images/UpstairsHall/UpstairsHall04_10-14-21.JPG"
+import UpstairsHall05 from "../images/UpstairsHall/UpstairsHall05_10-14-21.JPG"
+
+// section images - master bedroom (10/14/2021)
+import MasterBedroom01 from "../images/MasterBedroom/MasterBedroom01_10-14-21.JPG"
+import MasterBedroom02 from "../images/MasterBedroom/MasterBedroom02_10-14-21.JPG"
+import MasterBedroom03 from "../images/MasterBedroom/MasterBedroom03_10-14-21.JPG"
+import MasterBedroom04 from "../images/MasterBedroom/MasterBedroom04_10-14-21.JPG"
+import MasterBedroom05 from "../images/MasterBedroom/MasterBedroom05_10-14-21.JPG"
+import MasterBedroom06 from "../images/MasterBedroom/MasterBedroom06_10-14-21.JPG"
+import MasterBedroom07 from "../images/MasterBedroom/MasterBedroom07_10-14-21.JPG"
+import MasterBedroom08 from "../images/MasterBedroom/MasterBedroom08_10-14-21.JPG"
+import MasterBedroom09 from "../images/MasterBedroom/MasterBedroom09_10-14-21.JPG"
+import MasterBedroom10 from "../images/MasterBedroom/MasterBedroom10_10-14-21.JPG"
+import MasterBedroom11 from "../images/MasterBedroom/MasterBedroom11_10-14-21.JPG"
+
+// section images - upstairs bathroom (10/14/2021)
+import UpstairsBath01 from "../images/UpstairsBath/UpstairsBath01_10-14-21.JPG"
+import UpstairsBath02 from "../images/UpstairsBath/UpstairsBath02_10-14-21.JPG"
+import UpstairsBath03 from "../images/UpstairsBath/UpstairsBath03_10-14-21.JPG"
+import UpstairsBath04 from "../images/UpstairsBath/UpstairsBath04_10-14-21.JPG"
+import UpstairsBath05 from "../images/UpstairsBath/UpstairsBath05_10-14-21.JPG"
+
+// section images - guest bedroom (10/14/2021)
+import Guest01 from "../images/Guest/Guest01_10-14-21.JPG"
+import Guest02 from "../images/Guest/Guest02_10-14-21.JPG"
+import Guest03 from "../images/Guest/Guest03_10-14-21.JPG"
+import Guest04 from "../images/Guest/Guest04_10-14-21.JPG"
+
+// section images - upstairs storage (10/14/2021)
+import Storage01 from "../images/Storage/Storage01_10-14-21.JPG"
+import Storage02 from "../images/Storage/Storage02_10-14-21.JPG"
+import Storage03 from "../images/Storage/Storage03_10-14-21.JPG"
+import Storage04 from "../images/Storage/Storage04_10-14-21.JPG"
+import Storage05 from "../images/Storage/Storage05_10-14-21.JPG"
 
 
 const month = 10;
-const day = 4;
+const day = 14;
 const year = 2021;
 
 const roomInfo = (floor, name, blueprintPicture, sectionPictures) => {
@@ -60,19 +210,30 @@ const sectionInfo = (room, picture) => {
    };
 };
 
+const outside = {
+   id: "Outside",
+   name: "Outside",
+   floorData: [
+      roomInfo(1, "North-Side", OutsideNorth, [OutsideNorth01]),
+      roomInfo(1, "East-Side", OutsideEast, [OutsideEast01, OutsideEast02, OutsideEast03, OutsideEast04, OutsideEast05, OutsideEast06]),
+      roomInfo(1, "South-Side", OutsideSouth, [OutsideSouth01, OutsideSouth02, OutsideSouth03, OutsideSouth04, OutsideSouth05, OutsideSouth06, OutsideSouth07]),
+      roomInfo(1, "West-Side", OutsideWest, [OutsideWest01, OutsideWest02]),
+   ]
+}
+
 const firstFloor = {
    id: "First_Floor",
    name: "First Floor",
    floorData: [ 
-      // roomInfo(1, "Garage", F1_Garage, [test1, test2, test3, test4]),
-      // roomInfo(1, "Entry", F1_Entry, [test1, test2, test3, test4]),
+      roomInfo(1, "Garage", F1_Garage, [Garage01, Garage02, Garage03, Garage04, Garage05, Garage06, Garage07, Garage08, Garage09, Garage10, Garage11, Garage12]),
+      roomInfo(1, "Entry", F1_Entry, [Entry01, Entry02]),
       roomInfo(1, "Kitchen", F1_Kitchen, [Kitchen01, Kitchen02, Kitchen03, Kitchen04, Kitchen05, Kitchen06, Kitchen07]),
-      // roomInfo(1, "Dining Room", F1_Dining, [test1, test2, test3, test4]),
-      // roomInfo(1, "Living Room", F1_Living, [test1, test2, test3, test4]),
-      // roomInfo(1, "Kitchen Hall/Laundry", F1_Laundry, [ test1, test2, test3, test4, ]),
-      // roomInfo(1, "Downstairs Office", F1_Office, [test1, test2, test3, test4]),
-      // roomInfo(1, "Downstairs Bathroom", F1_Bath, [test1, test2, test3, test4]),
-      // roomInfo(1, "Stairs", F1_Stairs, [test1, test2, test3, test4]),
+      roomInfo(1, "Dining Room", F1_Dining, [DiningRoom01, DiningRoom02, DiningRoom03, DiningRoom04, DiningRoom05, DiningRoom06, DiningRoom07, DiningRoom08, DiningRoom09, DiningRoom10, DiningRoom11, DiningRoom12, DiningRoom13, DiningRoom14 ]),
+      roomInfo(1, "Living Room", F1_Living, [ LivingRoom01, LivingRoom02, LivingRoom03, LivingRoom04, LivingRoom05, LivingRoom06, LivingRoom07, LivingRoom08, LivingRoom09, LivingRoom10, LivingRoom11, LivingRoom12, LivingRoom13, LivingRoom14, LivingRoom15, LivingRoom16, LivingRoom17, LivingRoom18, LivingRoom19, LivingRoom20, LivingRoom21 ]),
+      roomInfo(1, "Kitchen Hall/Laundry", F1_Laundry, [ DownstairsHall01, DownstairsHall02, DownstairsHall03, DownstairsHall04, DownstairsHall05, DownstairsHall06, DownstairsHall07, DownstairsHall08, DownstairsHall09 ]),
+      roomInfo(1, "Downstairs Office", F1_Office, [Office01, Office02, Office03, Office04, Office05, Office06, Office07, Office08]),
+      roomInfo(1, "Downstairs Bathroom", F1_Bath, [DownstairsBath01, DownstairsBath02, DownstairsBath03, DownstairsBath04, DownstairsBath05]),
+      roomInfo(1, "Stairs", F1_Stairs, [Stairs01, Stairs02, Stairs03, Stairs04, Stairs05, Stairs06, Stairs07]),
    ],
 };
 
@@ -80,12 +241,12 @@ const secondFloor = {
    id: "Second_Floor",
    name: "Second Floor",
    floorData: [
-      roomInfo(2, "Upstairs Hallway", F2_Hall, [test1, test2, test3, test4]),
-      // roomInfo(2, "Master Bedroom", F2_MBed, [test1, test2, test3, test4]),
-      // roomInfo(2, "Master Bathroom", F2_Bath, [test1, test2, test3, test4]),
-      // roomInfo(2, "Guest Bedroom", F2_Bed, [test1, test2, test3, test4]),
-      // roomInfo(2, "Guest Bedroom Space", F2_Storage, [ test1, test2, test3, test4, ]),
+      roomInfo(2, "Upstairs Hallway", F2_Hall, [UpstairsHall01, UpstairsHall02, UpstairsHall03, UpstairsHall04, UpstairsHall05]),
+      roomInfo(2, "Master Bedroom", F2_MBed, [MasterBedroom01, MasterBedroom02, MasterBedroom03, MasterBedroom04, MasterBedroom05, MasterBedroom06, MasterBedroom07, MasterBedroom08, MasterBedroom09, MasterBedroom10, MasterBedroom11]),
+      roomInfo(2, "Upstairs Bathroom", F2_Bath, [UpstairsBath01, UpstairsBath02, UpstairsBath03, UpstairsBath04, UpstairsBath05]),
+      roomInfo(2, "Guest Bedroom", F2_Bed, [Guest01, Guest02, Guest03, Guest04]),
+      roomInfo(2, "Guest Bedroom Space", F2_Storage, [ Storage01, Storage02, Storage03, Storage04, Storage05 ]),
    ],
 };
 
-export const houseData = [firstFloor, secondFloor];
+export const houseData = [outside, firstFloor, secondFloor];
